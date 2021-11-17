f2aa20e62d7922bb0495183831a9bfe5b761f2cd
Emily <emychavz@gmail.com>
2021-11-16 19:19:37 -0800

    Adding Levers for Level1

---
+++ TheSnatcher/Assets/Scripts/Lever.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public bool IsOpened { get; set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsOpened = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
